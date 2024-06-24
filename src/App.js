693747904a0e2c07bfc53f85e8de0c42966cd4fa
trena <20079502+tholst952@users.noreply.github.com>
2024-06-24 12:15:08 -0700

    refactored code, used derived state

--- src/App.js
+++ src/App.js
@@ -2,33 +2,51 @@ import "./index.css";
 import { useState } from "react";
 
 export default function App() {
-  const [bill, setBill] = useState(0);
-  const [tip, setTip] = useState(0);
-  const [friendTip, setFriendTip] = useState(0);
+  return (
+    <div>
+      <TipCalculator />
+    </div>
+  );
+}
+
+function TipCalculator() {
+  const [bill, setBill] = useState("");
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
+
+  // derived state
+  const tip = bill * ((percentage1 + percentage2) / 2 / 100).toFixed(2);
+
+  function handleReset() {
+    setBill("");
+    setPercentage1(0);
+    setPercentage2(0);
+  }
 
   return (
     <div>
       <BillInput bill={bill} onBillChange={setBill} />
+
       <SelectPercentage
-        tip={tip}
-        onTipChange={setTip}
-        friendTip={friendTip}
-        onFriendTipChange={setFriendTip}
+        percentage={percentage1}
+        onSetPercentage={setPercentage1}
       >
-        <option value={0}>Dissatisfied (0%)</option>
-        <option value={0.5 * 100}>It was okay (5%)</option>
-        <option value={0.1 * 100}>It was good (10%)</option>
-        <option value={0.2 * 100}>Absolutely amazing! (20%)</option>
+        <span>How did you like the service?</span>
       </SelectPercentage>
-      <OutputSummary bill={bill} tip={tip} friendTip={friendTip} />
-      <Reset
-        bill={bill}
-        onBillChange={setBill}
-        tip={tip}
-        onTipChange={setTip}
-        friendTip={friendTip}
-        onFriendTipChange={setFriendTip}
-      />
+
+      <SelectPercentage
+        percentage={percentage2}
+        onSetPercentage={setPercentage2}
+      >
+        <span>How did your friend like the service?</span>
+      </SelectPercentage>
+
+      {bill > 0 && (
+        <>
+          <OutputSummary bill={bill} tip={tip} />
+          <Reset onHandleReset={handleReset} />
+        </>
+      )}
     </div>
   );
 }
@@ -36,7 +54,7 @@ export default function App() {
 function BillInput({ bill, onBillChange }) {
   return (
     <div>
-      <span>How much was the bill?</span>
+      <label>How much was the bill?</label>
       <input
         type="text"
         value={bill}
@@ -46,68 +64,32 @@ function BillInput({ bill, onBillChange }) {
   );
 }
 
-function SelectPercentage({
-  tip,
-  onTipChange,
-  friendTip,
-  onFriendTipChange,
-  children,
-}) {
+function SelectPercentage({ percentage, onSetPercentage, children }) {
   return (
     <div>
-      <span>How did you like the service?</span>
+      <label>{children}</label>
       <select
         type="select"
-        value={tip}
-        onChange={(e) => onTipChange(Number(e.target.value))}
+        value={percentage}
+        onChange={(e) => onSetPercentage(Number(e.target.value))}
       >
-        {children}
-      </select>
-
-      <FriendPercentage
-        friendTip={friendTip}
-        onFriendTipChange={onFriendTipChange}
-        optionChildren={children}
-      />
-    </div>
-  );
-}
-
-function FriendPercentage({ friendTip, onFriendTipChange, optionChildren }) {
-  return (
-    <div>
-      <span>How did your friend like the service?</span>
-      <select
-        type="select"
-        value={friendTip}
-        onChange={(e) => onFriendTipChange(Number(e.target.value))}
-      >
-        {optionChildren}
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely amazing! (20%)</option>
       </select>
     </div>
   );
 }
 
-function OutputSummary({ bill, tip, friendTip }) {
-  function calcAverage(tip1, tip2) {
-    return (tip1 + tip2) / 2;
-  }
-
+function OutputSummary({ bill, tip }) {
   return (
     <h3>
-      You pay ${bill + calcAverage(tip, friendTip)} (${bill} +{" "}
-      {calcAverage(tip, friendTip)}
-      %)
+      You pay ${bill + tip} (${bill} + ${tip.toFixed(2)} tip)
     </h3>
   );
 }
 
-function Reset({ onBillChange, onTipChange, onFriendTipChange }) {
-  function handleReset() {
-    onBillChange(0);
-    onTipChange(0);
-    onFriendTipChange(0);
-  }
-
-  return <button onClick={() => handleReset()}>Reset</button>;
+function Reset({ onHandleReset }) {
+  return <button onClick={() => onHandleReset()}>Reset</button>;
 }
